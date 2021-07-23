fbb5d59f0a3ca24ce0befbdeb5486b1f2424d942
PlugDev <plugdev@outlook.com>
2021-07-23 17:05:36 +0200

    Update/JavaDoc/Fix/Rewrite commit
    Updated nearly every class.
    Added JavaDocs to every class.
    Fixed some flaws in the code.
    Rewrote some incorrect javause. (keyword: Java-Conventions)

--- src/com/iffyspeak/commands/SampleCommand.java
+++ src/com/iffyspeak/commands/SampleCommand.java
@@ -5,11 +5,30 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class SampleCommand implements CommandExecutor {
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
-        Player p = (Player) sender;
-        p.sendMessage("Test");
-        return true;
-    }
+public final class SampleCommand implements CommandExecutor {
+
+	/**
+	 * 
+	 * The method onCommand() will be "started", if executing a command. ~ simple,
+	 * right?
+	 * 
+	 * @author Sonny J.
+	 * @contributor PlugDev
+	 * @version 1.0
+	 * @since 1.0
+	 * 
+	 */
+
+	@Override
+	public boolean onCommand(final CommandSender commandSender, final Command command, final String string,
+			final String[] args) {
+		if (!(commandSender instanceof Player)) {
+			commandSender.sendMessage("§cThis command can only run a valid player.");
+			return true;
+		}
+
+		Player player = (Player) commandSender;
+		player.sendMessage("Test");
+		return true;
+	}
 }
