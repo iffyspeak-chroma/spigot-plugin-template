e794a170149e677e1c05f40c81c9e692f28eb11e
BuildTools <unconfigured@null.spigotmc.org>
2021-07-23 06:32:19 -0500

    Added an event listener

---
+++ src/com/iffyspeak/events/EventListner.java
@@ -0,0 +1,14 @@
+package com.iffyspeak.events;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class EventListner implements Listener {
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        // Some code here for when the player joins.
+    }
+
+}
--- src/com/iffyspeak/plugin/Main.java
+++ src/com/iffyspeak/plugin/Main.java
@@ -2,6 +2,7 @@ package com.iffyspeak.plugin;
 
 import com.iffyspeak.commands.SampleCommand;
 import com.iffyspeak.commands.SampleCommandTab;
+import com.iffyspeak.events.EventListner;
 import com.iffyspeak.tools.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,6 +13,7 @@ public class Main extends JavaPlugin {
         new Logger("Plugin template has been enabled.", Logger.LogType.info);
         getCommand("sample").setExecutor(new SampleCommand());
         getCommand("sample").setTabCompleter(new SampleCommandTab());
+        getServer().getPluginManager().registerEvents(new EventListner(), this);
     }
 
     @Override
